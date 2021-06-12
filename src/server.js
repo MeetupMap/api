0d61ad8983fb35b4a4a9ced34e208e66ad1fd3dc
warren yun <xxwarrenhdxx@gmail.com>
2021-06-11 22:53:04 -0400

    Create mongoDB database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
+const cors = require('cors');
+
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema.js');
 
+const mongoose = require('mongoose');
+const secrets = require('../secrets.json');
+
 const app = express();
-
-
+app.use(cors());
 app.use(
     '/graphql',
     graphqlHTTP({
@@ -12,5 +16,12 @@ app.use(
       graphiql: true,
     }),
   );  
+
+const uri = secrets.atlas_uri;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("connected to mongoDB database successfully");
+});
 
 app.listen(4000, () => {console.log("started server on port 4000")});
