19d21006d9b5ff7fc2e53d38ecb6ac15f6bc358a
warren yun <xxwarrenhdxx@gmail.com>
2021-09-04 11:39:18 -0400

    Finish prisma schema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const cors = require('cors');
-
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const { PrismaClient } = require('@prisma/client')
 const schema = require('./schema.js');
 
-const mongoose = require('mongoose');
-const secrets = require('../secrets.json');
 
 const app = express();
 app.use(cors());
@@ -17,7 +15,7 @@ app.use(
     }),
   );  
 
-const uri = secrets.atlas_uri;
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {console.log(`started server on port ${port}`)});
