73c2066318e9ea56566092e95d8cddf2107dc58a
warren yun <xxwarrenhdxx@gmail.com>
2021-09-04 15:43:59 -0400

    Test adding new user using prisma

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ const schema = require('./schema.js');
 
 
 const app = express();
+const prisma = new PrismaClient() 
+
+async function test() {
+
+  const newUser = await prisma.user.create({
+    data: {
+      id: "1",
+      name: "test1",
+      email: "test@gmail.com",
+    }
+  })
+
+  const users = await prisma.user.findMany()
+  console.log(users)
+  console.log("hello")
+}
+test()
+
 app.use(cors());
 app.use(
     '/graphql',
