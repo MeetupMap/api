e6c5af2f2731726b687f1a1837694509f2cf7bb5
warren yun <xxwarrenhdxx@gmail.com>
2021-09-08 09:55:48 -0400

    Clean up code

--- schemas/group.js
+++ schemas/group.js
@@ -5,7 +5,8 @@ const Resolver = {
     Query: {
         group: (_, {groupID}) => {
             return prisma.group.findUnique({
-                where: { id: groupID }
+                where: { id: groupID },
+                include: { meetups: true }
             });
         },
     },
@@ -13,24 +14,10 @@ const Resolver = {
     Mutation: {
         createGroup: async (_, {groupID, name, userID}) => {
             // Create group
-            await prisma.group.create({
+            return prisma.group.create({
                 data: {
                     id: groupID,
-                    name: name
-                },
-                include: {users: true, meetups: true }
-            });
-
-            // Grab the list of users from the new group to update
-            const group = await prisma.group.findUnique({
-                where: { id: groupID },
-                include: { users: true }
-            });
-
-            // Update group with creator of group
-            await prisma.group.update({
-                where: { id: groupID },
-                data: {
+                    name: name,
                     users: {
                         connect: {
                             id: userID
@@ -38,20 +25,6 @@ const Resolver = {
                     }
                 }
             });
-            
-            // Update list of groups for user            
-            await prisma.user.update({
-                where: { id: userID },
-                data: {
-                    groups: {
-                        connect: {
-                            id: groupID
-                        }
-                    }
-                }
-            });
-
-            return group;
         },    
         addUserToGroup: async(_, {groupID, userID}) => {
             const group = await prisma.group.update({
@@ -79,7 +52,7 @@ const Resolver = {
             return group;
         },
         removeUserFromGroup: async(_, {groupID, userID}) => {
-            const group = await prisma.group.update({
+            return prisma.group.update({
                 where: { id: groupID },
                 data: {
                     users: {
@@ -90,19 +63,6 @@ const Resolver = {
                 }
             });
 
-            await prisma.user.update({
-                where: { id: userID },
-                data: {
-                    groups: {
-                        disconnect: {
-                            id: groupID
-                        }
-                    }
-                }
-
-            });
-
-            return group;
         },
         deleteGroup: async(_, {groupID}) => {
             return prisma.group.delete({
